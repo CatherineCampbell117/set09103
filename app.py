b3c53a3c19157076dbf35466b27251acd6dc2489
Catherine Campbell <catherinecampbell117@gmail.com>
2022-10-20 22:05:10 +0100

    quote changed to double quotes at route

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-        return 'This is my first API call!'
-        
+    return 'This is my first API call!'
