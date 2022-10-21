fbfff1b58b73f7ffa0cb2d9c909e65d4a578ede5
Catherine Campbell <catherinecampbell117@gmail.com>
2022-10-21 12:15:01 +0100

    delete app.py and update hello.py with POST

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello Napier!"
+def hello_world():
+    return "This is my first API call!"
+
+@app.route ("/post", methods=["POST"])
+def testpost():
+    input_json = request.get_json(force=True)
+    dictToReturn = {'text':input_json['text']}
+    return jsonify(dictToReturn)
